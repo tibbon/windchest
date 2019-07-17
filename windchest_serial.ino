3615ba57bf3d703cd5bc781d67b85c7f3c285934
David Fisher <david@actbluetech.com>
2019-07-16 20:27:44 -0400

    Add files via upload

---
+++ windchest_serial.ino
@@ -0,0 +1,37 @@
+/*
+  Windchest Pad Sensor
+  Hook up each DDR pad sensor to a 5V line, and the other side to an analog input
+  Around a pound or so of force will trigger the switch on right in the middle
+
+  Built for an Arduino Mega, but that's just because we need so many analog ports to skip
+  additional hardware for debouncing noise on the switch
+
+  Use Touchdesigner to read the serial port
+*/
+
+int sensorValue = 0;        
+static const uint8_t analog_pins[] = {A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14};
+size_t number_pins = sizeof(analog_pins)/sizeof(analog_pins[0]);
+
+void setup() {
+  // initialize serial communications at 57600 bps
+  Serial.begin(57600);
+}
+
+void loop() {
+  for(int pin = 0; pin < number_pins; pin++) {
+    // read the analog in value:
+    sensorValue = analogRead(analog_pins[pin]);
+    if (sensorValue == 1024) {
+      Serial.print("p = ");
+      Serial.print(pin);
+      Serial.print("\t v = ");
+      Serial.println(sensorValue);
+    } else {
+      Serial.print("p = ");
+      Serial.print(pin);
+      Serial.print("\t v = ");
+      Serial.println(0);
+    }
+  }
+}
