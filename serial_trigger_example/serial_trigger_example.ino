2cd98ac03cc4e650c1ae4ab45a65afaf06d8368b
David Fisher <david@actbluetech.com>
2019-08-04 18:47:13 -0400

    Lots of Saturday work. On v2 of main file

---
+++ serial_trigger_example/serial_trigger_example.ino
@@ -0,0 +1,28 @@
+/*
+  Use this with an Arduino to send serial data over for processing in the example
+  Not what we'll use in the end; but this gives us real serial data we can work with
+  Probably way more people "jumping" on it than would normally happen. 
+
+  You'll have to set the input serial device in TD to something like `/dev/cu.usbSerial-14120`
+*/
+
+const int num_inputs = 15;
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+
+  randomSeed(analogRead(0));
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  delay(1000);
+  for(int x = 0; x < num_inputs; x++) {
+    Serial.print(random(0,2)); // Why it's 0-2? I dunno
+    Serial.print(","); // Comma separates the serial
+  }
+   
+  Serial.print('\n'); // Does a newline
+
+}
